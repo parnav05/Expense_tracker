1ed03571dd5a549b3316baf32c55eda8a790fe8e
parnav <parnavdev05@gmail.com>
2026-06-06 20:27:12 +0000

    file path modified

--- backend/src/middleware/errorHandler.js
+++ backend/src/middleware/errorHandler.js
@@ -1,6 +1,6 @@
 const logger = require('../utils/logger');
 
-const errorHandler = (err, req, res, next) => {
+const errorHandler = (err, req, res,) => {
   logger.error(`${err.name}: ${err.message}`, { stack: err.stack });
 
   // Sequelize validation error
